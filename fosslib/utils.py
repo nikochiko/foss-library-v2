ce5de4085794e2aff10696e9d5e9339daff449fd
Kaustubh Maske Patil <kaustubh299792458@gmail.com>
2021-09-16 01:16:51 +0530

    Add utils to convert between isbn10 and isbn13 with tests

---
+++ fosslib/utils.py
@@ -0,0 +1,84 @@
+def convert_isbn10_to_isbn13(isbn10: str) -> str:
+    """
+    Utility function for converting an ISBN-10 code to an ISBN-13 code
+
+    https://isbn-information.com/convert-isbn-10-to-isbn-13.html
+    """
+    assert len(isbn10) == 10, "ISBN-10 must be a 10-digit string"
+
+    partial_isbn13 = "978" + isbn10[:9]
+
+    check_digit = get_check_digit_for_isbn13(partial_isbn13)
+
+    isbn13 = partial_isbn13 + check_digit
+
+    return isbn13
+
+
+def convert_isbn13_to_isbn10(isbn13: str) -> str:
+    """
+    Utility function for converting an ISBN-13 code to an ISBN-10 code
+    """
+    assert len(isbn13) == 13, "ISBN-13 must be a 13-digit string"
+    assert isbn13.startswith("978"), "ISBN-13 must start with Bookland 978"
+
+    partial_isbn10 = isbn13[3:12]
+
+    check_digit = get_check_digit_for_isbn10(partial_isbn10)
+
+    isbn10 = partial_isbn10 + check_digit
+
+    return isbn10
+
+
+def get_check_digit_for_isbn13(partial_isbn13: str) -> str:
+    """
+    Gets the check digit for an incomplete ISBN-13 code
+
+    This check digit is the last digit of an ISBN code
+    """
+    sum_ = 0
+    multiply_3 = False
+
+    for ch in partial_isbn13[:12]:
+        to_add = int(ch)
+
+        if multiply_3:
+            to_add *= 3
+
+        sum_ += to_add
+
+        # we need to multiply with 3 alternately, hence toggling this bool
+        multiply_3 = not multiply_3
+
+    intermediate_digit = sum_ % 10
+    check_digit = (10 - intermediate_digit) % 10
+
+    check_digit = str(check_digit)
+
+    return check_digit
+
+
+def get_check_digit_for_isbn10(partial_isbn10: str) -> str:
+    """
+    Gets the check digit for an incomplete ISBN-10 code
+
+    This check digit is the last digit of an ISBN code
+    """
+    sum_ = 0
+    multiplier = 10
+
+    for ch in partial_isbn10[:9]:
+        sum_ += int(ch) * multiplier
+
+        multiplier -= 1
+
+    intermediate_digit = sum_ % 11
+    check_digit = (11 - intermediate_digit) % 11
+
+    if check_digit == 10:
+        check_digit = "X"
+    else:
+        check_digit = str(check_digit)
+
+    return check_digit
---
+++ tests/test_utils.py
@@ -0,0 +1,29 @@
+from fosslib.utils import convert_isbn10_to_isbn13, convert_isbn13_to_isbn10
+
+
+def test_isbn10_to_isbn13():
+    # test case 1
+    isbn10 = "0345391802"
+    isbn13 = "9780345391803"
+
+    assert convert_isbn10_to_isbn13(isbn10) == isbn13
+
+    # test case 2
+    isbn10 = "0061122416"
+    isbn13 = "9780061122415"
+
+    assert convert_isbn10_to_isbn13(isbn10) == isbn13
+
+
+def test_isbn13_to_isbn10():
+    # test case 1
+    isbn13 = "9780007220854"
+    isbn10 = "0007220855"
+
+    assert convert_isbn13_to_isbn10(isbn13) == isbn10
+
+    # test case 2
+    isbn13 = "9781449340377"
+    isbn10 = "1449340377"
+
+    assert convert_isbn13_to_isbn10(isbn13) == isbn10
