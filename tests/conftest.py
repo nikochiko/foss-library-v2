edf02242704c28c6053cd64679ccb1781e3507a0
Kaustubh Maske Patil <kaustubh299792458@gmail.com>
2021-09-22 16:19:21 +0530

    Modify tests for Circle CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from testing.postgresql import Postgresql
 
@@ -6,18 +8,29 @@ from fosslib.app import create_app, db
 
 @pytest.fixture
 def app():
-    with Postgresql() as postgres:
-        db_url = postgres.url()
+    if os.getenv("TEST_ENV") == "CIRCLECI":
+        # on circleci, a database container is used
+        # we should take the DB config from there
+        db_url = os.getenv("DATABASE_URL")
+    else:
+        # locally, we should create a new database for local testing
+        _postgres_db = Postgresql()
+        db_url = _postgres_db.url()
 
+    try:
         app = create_app(
             "tests.settings",
-            overrides={"SQLALCHEMY_DATABASE_URI": db_url},
+            overrides={"SQLALCHEMY_DATABASE_URI": db_url}
         )
 
         with app.app_context():
             db.create_all()
 
         yield app
+
+    finally:
+        if os.getenv("TEST_ENV") != "CIRCLECI":
+            _postgres_db.stop()
 
 
 @pytest.fixture
