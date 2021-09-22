73ccda40d22c231e7901aae47f6d4ed13f6b1dd2
Kaustubh Maske Patil <kaustubh299792458@gmail.com>
2021-09-22 15:17:45 +0530

    Rename members table

--- fosslib/members/models.py
+++ fosslib/members/models.py
@@ -2,9 +2,13 @@ from fosslib.database import CRUDMixin, db
 
 
 class Member(db.Model, CRUDMixin):
+    __tablename__ = "members"
+
     id = db.Column(db.Integer, primary_key=True)
+
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
     email = db.Column(db.String, unique=True)
+
     gravatar_url = db.Column(db.String)
     dues_paid = db.Column(db.Integer, default=0)
